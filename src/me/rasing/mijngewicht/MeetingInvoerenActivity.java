077d59e68d7ad5571c49e554ea11e21df2533eb3
Bastiaan Rasing <bastiaan@rasing.me>
2013-07-02 08:35:03 +0200

    Fixes #9

--- src/me/rasing/mijngewicht/MeetingInvoerenActivity.java
+++ src/me/rasing/mijngewicht/MeetingInvoerenActivity.java
@@ -1,10 +1,14 @@
 package me.rasing.mijngewicht;
 
 import me.rasing.mijngewicht.R;
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MeetingInvoerenActivity extends FragmentActivity {
 	public static final String ID = "id";
@@ -21,5 +25,37 @@ public class MeetingInvoerenActivity extends FragmentActivity {
 		fragmentManager.beginTransaction()
 			           .replace(R.id.container,  fragment)
 			           .commit();
+
+        // Inflate a "Done/Discard" custom action bar view.
+        LayoutInflater inflater = (LayoutInflater) getActionBar().getThemedContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View customActionBarView = inflater.inflate(
+                R.layout.actionbar_custom_view_done_discard, null);
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Done"
+                        finish(); // TODO: don't just finish()!
+                    }
+                });
+        customActionBarView.findViewById(R.id.actionbar_discard).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // "Discard"
+                        finish(); // TODO: don't just finish()!
+                    }
+                });
+
+        // Show the custom action bar view and hide the normal Home icon and title.
+        final ActionBar actionBar = getActionBar();
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM,
+                ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+                        | ActionBar.DISPLAY_SHOW_TITLE);
+        actionBar.setCustomView(customActionBarView, new ActionBar.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
 	}
 }
--- src/me/rasing/mijngewicht/NieuwemeetingFragment.java
+++ src/me/rasing/mijngewicht/NieuwemeetingFragment.java
@@ -8,7 +8,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import me.rasing.mijngewicht.R;
+import android.app.ActionBar;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
@@ -21,9 +21,6 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
@@ -35,22 +32,20 @@ import android.widget.TimePicker;
 
 public class NieuwemeetingFragment extends Fragment implements OnClickListener{
 	private int id = 0;
-	
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-    	setHasOptionsMenu(true);
-    	
-        View rootView = inflater.inflate(R.layout.fragment_nieuwemeeting, container, false);
-        
-        TextView editDate = (TextView) rootView.findViewById(R.id.editDate);
-        editDate.setOnClickListener(this);
-        TextView editTime = (TextView) rootView.findViewById(R.id.editTime);
-        editTime.setOnClickListener(this);
-		
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_nieuwemeeting, container, false);
+
+		TextView editDate = (TextView) rootView.findViewById(R.id.editDate);
+		editDate.setOnClickListener(this);
+		TextView editTime = (TextView) rootView.findViewById(R.id.editTime);
+		editTime.setOnClickListener(this);
+
 		final Intent intent = getActivity().getIntent();
 		id = intent.getIntExtra(MeetingInvoerenActivity.ID, 0);
-		
+
 		if (id != 0) {
 			final DbHelper mDbHelper = new DbHelper(getActivity().getBaseContext());
 			final SQLiteDatabase db = mDbHelper.getWritableDatabase();
@@ -62,7 +57,7 @@ public class NieuwemeetingFragment extends Fragment implements OnClickListener{
 					Metingen.COLUMN_NAME_GEWICHT,
 					Metingen.COLUMN_NAME_DATUM
 			};
-			
+
 			//final String[] values = {Integer.toString(id)};
 
 			// How you want the results sorted in the resulting Cursor
@@ -78,20 +73,20 @@ public class NieuwemeetingFragment extends Fragment implements OnClickListener{
 					null,                 // don't filter by row groups
 					Metingen.COLUMN_NAME_DATUM + " DESC"
 					);
-			
+
 			c.moveToFirst();
-	    	final double gewicht = c.getDouble(
-	    			c.getColumnIndexOrThrow(Metingen.COLUMN_NAME_GEWICHT)
-	    			);
-	    	final String datum = c.getString(
-	    			c.getColumnIndexOrThrow(Metingen.COLUMN_NAME_DATUM)
-	    			);
-	    	
-	    	db.close();
-	    	
+			final double gewicht = c.getDouble(
+					c.getColumnIndexOrThrow(Metingen.COLUMN_NAME_GEWICHT)
+					);
+			final String datum = c.getString(
+					c.getColumnIndexOrThrow(Metingen.COLUMN_NAME_DATUM)
+					);
+
+			db.close();
+
 			TextView mWeight = (TextView) rootView.findViewById(R.id.gewicht);
 			mWeight.setText(NumberFormat.getInstance().format(gewicht));
-			
+
 			SimpleDateFormat format = 
 					new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
 			try {
@@ -104,174 +99,192 @@ public class NieuwemeetingFragment extends Fragment implements OnClickListener{
 				e.printStackTrace();
 			}
 		} else {
-	        // Use the current date as the default date in the picker
+			// Use the current date as the default date in the picker
 			final Calendar c = Calendar.getInstance();
 			final Date today = c.getTime();
-	        
-	        editDate.setText(DateFormat.getDateInstance(DateFormat.LONG).format(today));
-	        editTime.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(today));
-	        
-	        getActivity().setTitle("Nieuwe meeting");
+
+			editDate.setText(DateFormat.getDateInstance(DateFormat.LONG).format(today));
+			editTime.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(today));
+
+			getActivity().setTitle("Nieuwe meeting");
 		}
-        
-        return rootView;
-    }
-    
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-    	inflater.inflate(R.menu.nieuwe_meeting_menu, menu);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	// handle item selection
-    	switch (item.getItemId()) {
-    	case R.id.action_done:
-			// Gets the data repository in write mode
-			DbHelper mDbHelper = new DbHelper(getActivity().getBaseContext());
-			SQLiteDatabase db = mDbHelper.getWritableDatabase();
-			
 
-			InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-			imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
 
-			EditText editText = (EditText) getActivity().findViewById(R.id.gewicht);
-			String gewicht = editText.getText().toString();
 
-			TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
-			String mDatum = editDate.getText().toString();
+		// Inflate a "Done/Discard" custom action bar view.
+		//LayoutInflater actionBarInflater = (LayoutInflater) getActivity().getActionBar().getThemedContext()
+		//        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		final View customActionBarView = inflater.inflate(
+				R.layout.actionbar_custom_view_done_discard, null);
+		customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// Gets the data repository in write mode
+						DbHelper mDbHelper = new DbHelper(getActivity().getBaseContext());
+						SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
-			TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
-			String mTijdstip = editTime.getText().toString();
 
-			try {
-				Date datum = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).parse(mDatum + " " + mTijdstip);
-				DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
+						InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+						imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
 
-				// Create a new map of values, where column names are the keys
-				ContentValues values = new ContentValues();
-				values.put(Metingen.COLUMN_NAME_GEWICHT, gewicht);
-				values.put(Metingen.COLUMN_NAME_DATUM, formatter.format(datum));
+						EditText editText = (EditText) getActivity().findViewById(R.id.gewicht);
+						String gewicht = editText.getText().toString();
 
-				if (id != 0) {
-					// Which row to update, based on the ID
-					String selection = Metingen._ID + "=?";
-					String[] selectionArgs = { String.valueOf(id) };
+						TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
+						String mDatum = editDate.getText().toString();
 
-					db.update(
-					    Metingen.TABLE_NAME,
-					    values,
-					    selection,
-					    selectionArgs);
-				} else {
-					// Insert the new row, returning the primary key value of the new row
-					db.insert(
-							Metingen.TABLE_NAME,
-							null,
-							values);
+						TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
+						String mTijdstip = editTime.getText().toString();
 
-				}
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+						try {
+							Date datum = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).parse(mDatum + " " + mTijdstip);
+							DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault());
 
-			db.close();
-    		
-    		getActivity().finish();
+							// Create a new map of values, where column names are the keys
+							ContentValues values = new ContentValues();
+							values.put(Metingen.COLUMN_NAME_GEWICHT, gewicht);
+							values.put(Metingen.COLUMN_NAME_DATUM, formatter.format(datum));
 
-    		return true;
-    	default:
-    		return super.onOptionsItemSelected(item);
-    	}
-    }
-    
+							if (id != 0) {
+								// Which row to update, based on the ID
+								String selection = Metingen._ID + "=?";
+								String[] selectionArgs = { String.valueOf(id) };
+
+								db.update(
+										Metingen.TABLE_NAME,
+										values,
+										selection,
+										selectionArgs);
+							} else {
+								// Insert the new row, returning the primary key value of the new row
+								db.insert(
+										Metingen.TABLE_NAME,
+										null,
+										values);
+
+							}
+						} catch (ParseException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+						db.close();
+						
+						getActivity().finish(); // TODO: don't just finish()!
+					}
+				});
+		customActionBarView.findViewById(R.id.actionbar_discard).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						// "Discard"
+						getActivity().finish(); // TODO: don't just finish()!
+					}
+				});
+
+		// Show the custom action bar view and hide the normal Home icon and title.
+		final ActionBar actionBar = getActivity().getActionBar();
+		actionBar.setDisplayOptions(
+				ActionBar.DISPLAY_SHOW_CUSTOM,
+				ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+				| ActionBar.DISPLAY_SHOW_TITLE);
+		actionBar.setCustomView(customActionBarView, new ActionBar.LayoutParams(
+				ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.MATCH_PARENT));
+
+
+
+		return rootView;
+	}
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-			case R.id.editDate:
-		    	DialogFragment newFragment = new DatePickerFragment();
-		    	newFragment.show(getFragmentManager(), "datePicker");
-				break;
-			case R.id.editTime:
-				DialogFragment timePicker = new TimePickerFragment();
-				timePicker.show(getFragmentManager(), "timePicker");
-				break;
+		case R.id.editDate:
+			DialogFragment newFragment = new DatePickerFragment();
+			newFragment.show(getFragmentManager(), "datePicker");
+			break;
+		case R.id.editTime:
+			DialogFragment timePicker = new TimePickerFragment();
+			timePicker.show(getFragmentManager(), "timePicker");
+			break;
 		}
 	}
 
-    public static class DatePickerFragment extends DialogFragment
-    implements DatePickerDialog.OnDateSetListener {
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    		TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
-    		String mDatum = editDate.getText().toString();
-			
-    		// Use the current date as the default date in the picker
-    		Calendar c = Calendar.getInstance();
-    		
+	public static class DatePickerFragment extends DialogFragment
+	implements DatePickerDialog.OnDateSetListener {
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
+			String mDatum = editDate.getText().toString();
+
+			// Use the current date as the default date in the picker
+			Calendar c = Calendar.getInstance();
+
 			try {
 				Date datum = DateFormat.getDateInstance(DateFormat.LONG).parse(mDatum);
-	    		c.setTime(datum);
+				c.setTime(datum);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-    		int year = c.get(Calendar.YEAR);
-    		int month = c.get(Calendar.MONTH);
-    		int day = c.get(Calendar.DAY_OF_MONTH);
 
-    		// Create a new instance of DatePickerDialog and return it
-    		return new DatePickerDialog(getActivity(), this, year, month, day);
-    	}
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
 
-    	public void onDateSet(DatePicker view, int year, int month, int day) {
-    		// Use the current date as the default date in the picker
-    		Calendar c = Calendar.getInstance();
-    		c.set(year, month, day);
-    		
-            TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
-            editDate.setText(DateFormat.getDateInstance(DateFormat.LONG).format(c.getTime()));
-    	}
-    }
-    
-    public static class TimePickerFragment extends DialogFragment
-    implements TimePickerDialog.OnTimeSetListener {
-		
-    	@Override
-    	public Dialog onCreateDialog(Bundle savedInstanceState) {
-    		TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
-    		String mTijdstip = editTime.getText().toString();
-    		
-    		// Use the current time as the default values for the picker
-    		final Calendar c = Calendar.getInstance();
-    		
+			// Create a new instance of DatePickerDialog and return it
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			// Use the current date as the default date in the picker
+			Calendar c = Calendar.getInstance();
+			c.set(year, month, day);
+
+			TextView editDate = (TextView) getActivity().findViewById(R.id.editDate);
+			editDate.setText(DateFormat.getDateInstance(DateFormat.LONG).format(c.getTime()));
+		}
+	}
+
+	public static class TimePickerFragment extends DialogFragment
+	implements TimePickerDialog.OnTimeSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
+			String mTijdstip = editTime.getText().toString();
+
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+
 			try {
 				Date tijdstip = DateFormat.getTimeInstance(DateFormat.SHORT).parse(mTijdstip);
-	    		c.setTime(tijdstip);
+				c.setTime(tijdstip);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-    		
-    		int hour = c.get(Calendar.HOUR_OF_DAY);
-    		int minute = c.get(Calendar.MINUTE);
 
-    		// Create a new instance of TimePickerDialog and return it
-    		return new TimePickerDialog(getActivity(), this, hour, minute,
-    				android.text.format.DateFormat.is24HourFormat(getActivity()));
-    	}
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
 
-    	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-    		// Use the current date as the default date in the picker
-    		Calendar c = Calendar.getInstance();
-    		c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-    		c.set(Calendar.MINUTE, minute);
-    		
-            TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
-            editTime.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime()));
-    	}
-    }
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute,
+					android.text.format.DateFormat.is24HourFormat(getActivity()));
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			// Use the current date as the default date in the picker
+			Calendar c = Calendar.getInstance();
+			c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			c.set(Calendar.MINUTE, minute);
+
+			TextView editTime = (TextView) getActivity().findViewById(R.id.editTime);
+			editTime.setText(DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime()));
+		}
+	}
 
 }
