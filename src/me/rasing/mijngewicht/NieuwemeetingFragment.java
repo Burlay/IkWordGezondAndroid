99afe8131434fa750ff734e47022cb51f178f607
Bastiaan Rasing <bastiaan@rasing.me>
2013-07-06 12:54:40 +0200

    versie opgehoogd naar 4/1.3

--- src/me/rasing/mijngewicht/NieuwemeetingFragment.java
+++ src/me/rasing/mijngewicht/NieuwemeetingFragment.java
@@ -20,7 +20,6 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
