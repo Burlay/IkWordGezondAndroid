43bd5613bfebae0a41098e442f0869a80c847f17
Bastiaan Rasing <bastiaan@rasing.me>
2013-08-11 12:20:09 +0200

    add contentprovider

--- src/me/rasing/mijngewicht/MainActivity.java
+++ src/me/rasing/mijngewicht/MainActivity.java
@@ -11,8 +11,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
---
+++ src/me/rasing/mijngewicht/providers/GewichtProvider.java
@@ -0,0 +1,83 @@
+package me.rasing.mijngewicht.providers;
+
+import me.rasing.mijngewicht.DbHelper;
+import me.rasing.mijngewicht.Metingen;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.util.Log;
+
+public class GewichtProvider extends ContentProvider {
+	private static final String AUTHORITY = "me.rasing.mijngewicht.providers.GewichtProvider";
+	public static final Uri METINGEN_URI = Uri.parse("content://" + AUTHORITY
+	        + "/" + Metingen.TABLE_NAME);
+	private static DbHelper mDbHelper;
+	private static final UriMatcher sUriMatcher;
+	private static final int METINGEN = 0;
+	private static final int METINGEN_ID = 1;
+	
+	@Override
+	public boolean onCreate() {
+		Log.d(this.getClass().toString(), "onCreate");
+		mDbHelper = new DbHelper(getContext());
+		
+		return true;
+	}
+	
+	@Override
+	public int delete(Uri arg0, String arg1, String[] arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getType(Uri arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Uri insert(Uri arg0, ContentValues arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+			String sortOrder) {
+		Log.d("URI", uri.toString());
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables(Metingen.TABLE_NAME);
+		
+		switch (sUriMatcher.match(uri)) {
+			case METINGEN:
+				break;
+			case METINGEN_ID:
+				Log.d("case", "Metingen_id");
+				break;
+			default:
+				Log.d("case", "not found");
+		}
+		
+		SQLiteDatabase db = mDbHelper.getReadableDatabase();
+		Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+		
+		c.setNotificationUri(getContext().getContentResolver(), uri);
+		return c;
+	}
+
+	@Override
+	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	static {
+        sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	    sUriMatcher.addURI(AUTHORITY, Metingen.TABLE_NAME, METINGEN);
+	    sUriMatcher.addURI(AUTHORITY, Metingen.TABLE_NAME + "/#", METINGEN_ID);
+	}
+}
