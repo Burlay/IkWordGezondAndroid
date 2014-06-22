9a57da8a3b9adc528a0156985057b85838f2b073
Bastiaan Rasing <bastiaan@rasing.me>
2014-06-22 11:05:34 +0200

    Move clicklistener to own function

--- src/me/rasing/mijngewicht/MeetingInvoerenActivity.java
+++ src/me/rasing/mijngewicht/MeetingInvoerenActivity.java
@@ -16,20 +16,22 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MeetingInvoerenActivity extends FragmentActivity {
+public class MeetingInvoerenActivity extends FragmentActivity implements OnClickListener {
 	public static final String ID = "id";
+	private NieuwemeetingFragment fragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_meeting_invoeren);
 		
-		final NieuwemeetingFragment fragment = new NieuwemeetingFragment();
+		fragment = new NieuwemeetingFragment();
 
 		// Insert the fragment by replacing any existing fragment
 		FragmentManager fragmentManager = getSupportFragmentManager();
@@ -42,72 +44,7 @@ public class MeetingInvoerenActivity extends FragmentActivity {
                 .getSystemService(LAYOUT_INFLATER_SERVICE);
         final View customActionBarView = inflater.inflate(
                 R.layout.actionbar_custom_view_done_discard, null);
-        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-						EditText editText = (EditText) findViewById(R.id.gewicht);
-						String gewicht = editText.getText().toString();
-
-						TextView editDate = (TextView) findViewById(R.id.editDate);
-						String mDatum = editDate.getText().toString();
-
-						TextView editTime = (TextView) findViewById(R.id.editTime);
-						String mTijdstip = editTime.getText().toString();
-
-						
-						if (gewicht.length() == 0) {
-							editText.setError("Vul je gewicht in.");
-							return;
-						}
-						
-						// Gets the data repository in write mode
-						DbHelper mDbHelper = new DbHelper(getBaseContext());
-						SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
-						InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-						imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-						try {
-							Date datum = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).parse(mDatum + " " + mTijdstip);
-							DateFormat formatter = new SimpleDateFormat(
-									"yyyy-MM-dd'T'HH:mmZ", Locale.getDefault());
-
-							// Create a new map of values, where column names are the keys
-							ContentValues values = new ContentValues();
-							values.put(Metingen.COLUMN_NAME_GEWICHT, gewicht);
-							values.put(Metingen.COLUMN_NAME_DATUM, formatter.format(datum));
-							
-							int id = fragment.getMeetingId();
-
-							if (id != 0) {
-								// Which row to update, based on the ID
-								String selection = Metingen._ID + "=?";
-								String[] selectionArgs = { String.valueOf(id) };
-
-								db.update(
-										Metingen.TABLE_NAME,
-										values,
-										selection,
-										selectionArgs);
-							} else {
-								// Insert the new row, returning the primary key value of the new row
-								values.put(Metingen.COLUMN_NAME_GUID, UUID.randomUUID().toString());
-								db.insert(
-										Metingen.TABLE_NAME,
-										null,
-										values);
-
-							}
-						} catch (ParseException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-
-						db.close();
-						
-						finish();
-                    }
-                });
+        customActionBarView.findViewById(R.id.actionbar_done).setOnClickListener(this);
         customActionBarView.findViewById(R.id.actionbar_discard).setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -125,5 +62,69 @@ public class MeetingInvoerenActivity extends FragmentActivity {
         actionBar.setCustomView(customActionBarView, new ActionBar.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT));
+	}
+
+	@Override
+	public void onClick(View v) {
+		EditText editText = (EditText) findViewById(R.id.gewicht);
+		String gewicht = editText.getText().toString();
+
+		TextView editDate = (TextView) findViewById(R.id.editDate);
+		String mDatum = editDate.getText().toString();
+
+		TextView editTime = (TextView) findViewById(R.id.editTime);
+		String mTijdstip = editTime.getText().toString();
+
+		
+		if (gewicht.length() == 0) {
+			editText.setError("Vul je gewicht in.");
+			return;
+		}
+		
+		// Gets the data repository in write mode
+		DbHelper mDbHelper = new DbHelper(getBaseContext());
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+		try {
+			Date datum = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT).parse(mDatum + " " + mTijdstip);
+			DateFormat formatter = new SimpleDateFormat(
+					"yyyy-MM-dd'T'HH:mmZ", Locale.getDefault());
+
+			// Create a new map of values, where column names are the keys
+			ContentValues values = new ContentValues();
+			values.put(Metingen.COLUMN_NAME_GEWICHT, gewicht);
+			values.put(Metingen.COLUMN_NAME_DATUM, formatter.format(datum));
+			
+			int id = fragment.getMeetingId();
+
+			if (id != 0) {
+				// Which row to update, based on the ID
+				String selection = Metingen._ID + "=?";
+				String[] selectionArgs = { String.valueOf(id) };
+
+				db.update(
+						Metingen.TABLE_NAME,
+						values,
+						selection,
+						selectionArgs);
+			} else {
+				// Insert the new row, returning the primary key value of the new row
+				values.put(Metingen.COLUMN_NAME_GUID, UUID.randomUUID().toString());
+				db.insert(
+						Metingen.TABLE_NAME,
+						null,
+						values);
+
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		db.close();
+		
+		finish();
 	}
 }
